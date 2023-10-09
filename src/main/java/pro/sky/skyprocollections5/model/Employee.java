e3b1263c6b4203215b42522497d93b4df50df463
Maksim <maks.kartashov.93@inbox.ru>
2023-10-09 20:29:28 +0200

    collections

--- src/main/java/pro/sky/skyprocollections5/model/Employee.java
+++ src/main/java/pro/sky/skyprocollections5/model/Employee.java
@@ -1,14 +1,37 @@
 package pro.sky.skyprocollections5.model;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class Employee {
     private String lstName;
     private String firstName;
+    private int salary;
+    private int department;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
 
     public Employee(String lstName, String firstName) {
+        Random random=new Random();
         this.lstName = lstName;
         this.firstName = firstName;
+        this.salary = random.nextInt(10000) + 1000;
+        this.department=random.nextInt(2)+1;
+
     }
 
     @Override
