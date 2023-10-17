e7154290d1acf28bea96abd301ef2481b1881f98
Maksim <maks.kartashov.93@inbox.ru>
2023-10-17 20:04:50 +0200

    валидация,исправление данных

--- src/main/java/pro/sky/skyprocollections5/service/EmployeeServiceimpl.java
+++ src/main/java/pro/sky/skyprocollections5/service/EmployeeServiceimpl.java
@@ -1,8 +1,10 @@
 package pro.sky.skyprocollections5.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import pro.sky.skyprocollections5.exception.EmployeeAlreadyAddedException;
 import pro.sky.skyprocollections5.exception.EmployeeNotFoundException;
+import pro.sky.skyprocollections5.exception.ValidateException;
 import pro.sky.skyprocollections5.model.Employee;
 
 import java.util.*;
@@ -19,6 +21,9 @@ public class EmployeeServiceimpl implements EmployeeService {
 
     @Override
     public Employee add(String firstName, String lastName) {
+        firstName = StringUtils.capitalize(firstName);
+        lastName = StringUtils.capitalize(lastName);
+        valideteFirstLastNames(firstName,lastName);
         Employee employee = new Employee(firstName, lastName);
         if (empolyees.containsKey(employee.getFullName()))
             throw new EmployeeAlreadyAddedException();
@@ -27,9 +32,11 @@ public class EmployeeServiceimpl implements EmployeeService {
             return employee;
         }
     }
-
     @Override
     public Employee remove(String firstName, String lastName) {
+        firstName = StringUtils.capitalize(firstName);
+        lastName = StringUtils.capitalize(lastName);
+        valideteFirstLastNames(firstName,lastName);
         Employee employee = new Employee(firstName, lastName);
         if (empolyees.containsKey(employee.getFullName())) {
             return empolyees.get(employee.getFullName());
@@ -42,6 +49,10 @@ public class EmployeeServiceimpl implements EmployeeService {
 
     @Override
     public Employee find(String firstName, String lastName) {
+        firstName = StringUtils.capitalize(firstName);
+        lastName = StringUtils.capitalize(lastName);
+        valideteFirstLastNames(firstName,lastName);
+
         Employee employee = new Employee(firstName, lastName);
         if (empolyees.containsKey(employee.getFullName())) {
             return empolyees.get(employee.getFullName());
@@ -52,6 +63,16 @@ public class EmployeeServiceimpl implements EmployeeService {
     public Collection<Employee> findAll() {
             return Collections.unmodifiableCollection(empolyees.values());
         }
+
+    private void valideteFirstLastNames(String firstName, String lastName) {
+        if (!StringUtils.isNumeric(firstName)) {
+            throw new ValidateException("Имя содержит запрещенные символы");
+        }
+        if (!StringUtils.isNumeric(lastName)) {
+            throw new ValidateException("Фамилия содержит запрещенные символы");
+        }
+
+    }
 
     }
 
